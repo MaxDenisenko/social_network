127845f59e15459ea24079853c58d7762b08e314
MaxDenisenko <MaxDenisenko2013@yandex.ru>
2023-04-12 11:44:12 +0300

    add typescript for API

--- src/Redux/AuthReducer.ts
+++ src/Redux/AuthReducer.ts
@@ -1,5 +1,5 @@
 import { stopSubmit } from 'redux-form';
-import { secureAPI, usersAPI } from '../components/api/Api';
+import { ResultCodesEnum, secureAPI, usersAPI } from '../components/api/Api';
 
 const SET_USER_DATA = 'SET_USER_DATA';
 const GET_CAPTCHA = 'GET_CAPTCHA';
@@ -56,7 +56,7 @@ export const getCaptchaUrlActionCreator = (captchaUrl: string): getCaptchaUrlAct
 export const getAuthUserDataThunkCreator = () => async (dispatch: any) => {
   let data = await usersAPI.getAuthDataUsers();
 
-  if (data.resultCode === 0) {
+  if (data.resultCode === ResultCodesEnum.Success) {
     let { id, email, login } = data.data;
     dispatch(setAuthUserDataActionCreator(id, email, login, true));
   }
@@ -65,10 +65,10 @@ export const getAuthUserDataThunkCreator = () => async (dispatch: any) => {
 export const loginThunkCreator = (email: string, password: string, rememberMe: boolean, captcha: null | undefined) => {
   return (dispatch: any) => {
     usersAPI.login(email, password, rememberMe, captcha).then((response) => {
-      if (response.data.resultCode === 0) {
+      if (response.data.resultCode === ResultCodesEnum.Success) {
         dispatch(getAuthUserDataThunkCreator());
       } else {
-        if (response.data.resultCode === 10) {
+        if (response.data.resultCode === ResultCodesEnum.CaptchaIsRequired) {
           dispatch(getCaptchaUrlThunkCreator());
         }
         let msg = response.data.messages.length > 0 ? response.data.messages[0] : 'Some error';
--- src/components/api/Api.ts
+++ src/components/api/Api.ts
@@ -1,47 +1,64 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+import authReducer from '../../Redux/AuthReducer';
 
 const instance = axios.create({
   baseURL: 'https://social-network.samuraijs.com/api/1.0/',
   withCredentials: true,
   headers: { 'API-KEY': '05eb6eff-6789-4df5-9de9-94f2a276a805' },
 });
+export enum ResultCodesEnum {
+  Success = 0,
+  Error = 1,
+  CaptchaIsRequired = 10,
+}
 
+type LoginResponseType2 = {
+  data: { userId: number };
+  resultCode: ResultCodesEnum;
+  messages: Array<string>;
+};
+
+type LoginResponseType = {
+  data: { id: number; email: string; login: string };
+  resultCode: ResultCodesEnum;
+  messages: Array<string>;
+};
 export const usersAPI = {
   getUsers(currentPage = 1, pageSize = 10) {
     return instance.get(`users?page=${currentPage}&count=${pageSize}`).then((response) => response.data);
   },
-  getProfileUsers(userId) {
+  getProfileUsers(userId: number) {
     console.warn('Please use new object profileAPI');
     return profileAPI.getProfileUsers(userId);
   },
   getAuthDataUsers() {
-    return instance.get(`auth/me/`).then((response) => response.data);
+    return instance.get<LoginResponseType>(`auth/me/`).then((response) => response.data);
   },
-  login(email, password, rememberMe = false, captcha = null) {
-    return instance.post(`auth/login`, { email, password, rememberMe, captcha });
+  login(email: string, password: string, rememberMe: boolean = false, captcha: null | string = null) {
+    return instance.post<LoginResponseType2>(`auth/login`, { email, password, rememberMe, captcha });
   },
   logout() {
     return instance.delete(`auth/login`);
   },
-  usersFollow(userId) {
+  usersFollow(userId: number) {
     return instance.delete(`follow/${userId}`).then((response) => response);
   },
-  usersUnFollow(userId) {
+  usersUnFollow(userId: number) {
     return instance.post(`follow/${userId}`).then((response) => response);
   },
 };
 
 export const profileAPI = {
-  getProfileUsers(userId) {
+  getProfileUsers(userId: number) {
     return instance.get(`profile/${userId}`).then((response) => response.data);
   },
-  getProfileStatus(userId) {
+  getProfileStatus(userId: number) {
     return instance.get(`profile/status/${userId}`);
   },
-  updateProfileStatus(status) {
+  updateProfileStatus(status: string) {
     return instance.put(`profile/status/`, { status: status });
   },
-  savePhoto(file) {
+  savePhoto(file: any) {
     let formData = new FormData();
     formData.append('image', file);
     return instance.put(`profile/photo/`, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
